99fca94b7e258af3a86e966d63bbcc1de3eb424a
Jose Gabriel Perea Medina <josegabrielduckie@gmail.com>
2020-10-12 20:14:36 +0200

    Ejemplo de salida de datos median lectura de un archivo html y mediante un json

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
 import http from 'http'
-import { compile } from 'morgan';
+//Importamos la clase FileStream con -> fs
+import fs from'fs';
 
 
 
-const host = 'localhost';
-const port = '8000';
-const server = http.createServer((request,response)=>{
+const host = 'localhost'
+const port = '8000'
+//Ruta del fichero
+const file = './src/index.html'
+const server = http.createServer((request, response) => {
+/**
+ * Mediante HTML
+ */
+    // response.writeHead(200, {
+    //     'Content-Type': 'text/html; charset=UTF-8'
+    // })
+    // //Para Leer el fichero Html 
+    // fs.readFile(file, (err, content) => {
+    //     if (err){
+    //         return console.log(err)
+    //     }
+    //     response.write(content)
+    //     return response.end();  
+    // })
+    //=======================================End HTML
+    /**
+     * MedianteJSON
+     */
+    response.writeHead(200, {
+        'Content-Type': 'application/json'
+    })
 
   
-
-    /**Metodo para enviar Las Cabeceras  */
-    response.setHeader('Content-Type','application/json')
-   
-    if(request.method==='GET'){
-        response.statusCode=300
-         
-        response.write("<h1>Este Metodo esta permitido</h1>")
-        response.end();
-    }
-    else{
-        //Ejemplo de codigo de estado
-        response.statusCode=404
-        response.write("Este Metdo no esta permitido")
-    }
+        response.write(JSON.stringify({'key':'Value'}))
+        return response.end();  
     
-   return response.end();
+
+
+
+
+    
 })
 
-server.listen(port,server,err=>{
-    if(err){
+server.listen(port, server, err => {
+    if (err) {
         return console.log(err)
     }
-    console.log('Servidor Abierto en la ruta http://'+host+':'+port)
+    console.log('Servidor Abierto en la ruta http://' + host + ':' + port)
 })
