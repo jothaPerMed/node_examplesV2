5c143a720814d2fa90d0d90633642c8bdf618834
Jose Gabriel Perea Medina <josegabrielduckie@gmail.com>
2020-10-13 13:39:17 +0200

    Utilizando Express para ficheros lectura de estaticos HTML / CSS / JS

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-/**
- * Vamos a crear un servidor en un puerto y a partir de hay vamos a generar la ruta raiz
- * Desde hay vamos a renderizar nuestro servidor.
- */
-//En 1º lugar importar dependencia de express.
-import express, {
-    response
-} from 'express'
-//Importar Morgan como logger ->> Servirá como middleware en las peticiones GET\POST\PUT...
-import morgan from 'morgan'
-//INSTALAR DEPENDENCIA BODYPARSER --> npm install --save body-parser
-import bodyParser from 'body-parser';
-//En 2º lugar cargar una instancia dentro de una veriable en este caso una CONSTANTE.
-const app = express()
-app.disable('x-powered-by') //Deshabilitar cabecera que hace referencia a express en la respuesta.
-app.set('env', 'development') //Entorno en el que estamos ejecutando el servidor.
-app.use(morgan('common')) //Utilizar Morgan
-app.use(bodyParser.json())//Utilizar como middleware para devolver en en request sobr5e todo las POST
-app.use(bodyParser.urlencoded({extended: false}))
+import express from 'express'
 
-//En 3º lugar generar las rutas
+import morgan from 'morgan'
+
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express()
+/**MIDDLEWARES */
+app.disable('x-powered-by')
+app.set('env', 'development')
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+/**END MIDDLEWARES */
+
 app.get('/', (request, response) => {
     response.end("Hola Mundo!")
 })
 
+/**
+ * Utilizar Ficheros estaticos parar leerlos segun su extension e importarlos
+ */
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 //En 4º lugar vamos a decirle a express que escuche en el puerto 9000 .
 app.listen('9000', () => {
