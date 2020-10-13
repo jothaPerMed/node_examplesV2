9ba4226fcf5d0663170675b07078305257a1e1cb
Jose Gabriel Perea Medina <josegabrielduckie@gmail.com>
2020-10-13 12:33:21 +0200

    Trabajando Con Express , Morgan , Body-Parser

--- src/index.js
+++ src/index.js
@@ -1,50 +1,30 @@
-import http from 'http'
-//Importamos la clase FileStream con -> fs
-import fs from'fs';
-
-
-
-const host = 'localhost'
-const port = '8000'
-//Ruta del fichero
-const file = './src/index.html'
-const server = http.createServer((request, response) => {
 /**
- * Mediante HTML
+ * Vamos a crear un servidor en un puerto y a partir de hay vamos a generar la ruta raiz
+ * Desde hay vamos a renderizar nuestro servidor.
  */
-    // response.writeHead(200, {
-    //     'Content-Type': 'text/html; charset=UTF-8'
-    // })
-    // //Para Leer el fichero Html 
-    // fs.readFile(file, (err, content) => {
-    //     if (err){
-    //         return console.log(err)
-    //     }
-    //     response.write(content)
-    //     return response.end();  
-    // })
-    //=======================================End HTML
-    /**
-     * MedianteJSON
-     */
-    response.writeHead(200, {
-        'Content-Type': 'application/json'
-    })
+//En 1º lugar importar dependencia de express.
+import express, {
+    response
+} from 'express'
+//Importar Morgan como logger ->> Servirá como middleware en las peticiones GET\POST\PUT...
+import morgan from 'morgan'
+//INSTALAR DEPENDENCIA BODYPARSER --> npm install --save body-parser
+import bodyParser from 'body-parser';
+//En 2º lugar cargar una instancia dentro de una veriable en este caso una CONSTANTE.
+const app = express()
+app.disable('x-powered-by') //Deshabilitar cabecera que hace referencia a express en la respuesta.
+app.set('env', 'development') //Entorno en el que estamos ejecutando el servidor.
+app.use(morgan('common')) //Utilizar Morgan
+app.use(bodyParser.json())//Utilizar como middleware para devolver en en request sobr5e todo las POST
+app.use(bodyParser.urlencoded({extended: false}))
 
-  
-        response.write(JSON.stringify({'key':'Value'}))
-        return response.end();  
-    
-
-
-
-
-    
+//En 3º lugar generar las rutas
+app.get('/', (request, response) => {
+    response.end("Hola Mundo!")
 })
 
-server.listen(port, server, err => {
-    if (err) {
-        return console.log(err)
-    }
-    console.log('Servidor Abierto en la ruta http://' + host + ':' + port)
+
+//En 4º lugar vamos a decirle a express que escuche en el puerto 9000 .
+app.listen('9000', () => {
+    console.log("Servidor Run.. PORT=>9000")
 })
