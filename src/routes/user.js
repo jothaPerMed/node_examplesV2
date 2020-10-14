e19a636bd02dceb2cae233919d983e6f8eb1258b
Jose Gabriel Perea Medina <josegabrielduckie@gmail.com>
2020-10-15 01:19:28 +0200

    Instalacion del paquete mysql y creacion de connexion

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,29 @@ import express from 'express'
 import {
     auth
 } from '../middleware'
+import db, {
+    connString,
+    dbconnetioninfo
+} from '../sqlconection'
+var mysql = require('mysql')
+//Creacion de la connexion
+const con = mysql.createConnection(connString)
 const router = express.Router()
 router
     .get('/', (req, res, next) => {
+
+        //Conectar
+        con.connect(function(err) {
+        if(err){
+            console.log({err})
+        }
+            console.log("Connected!");
+           
+          });
+         
+
         res
-            .statusCode(200)
+            .status(200)
             .json({
                 name: 'Jose Gabriel',
                 lastname: 'Perea Medina'
@@ -15,7 +33,7 @@ router
 
     })
 
-    .put('/',auth, (req, res, next) => {
+    .put('/', auth, (req, res, next) => {
 
         res
             .status(201)
---
+++ src/sqlconection.js
@@ -0,0 +1,10 @@
+
+// export let dbconnetioninfo =  {
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "prueba"
+// }
+
+export const connString = 'mysql://root:@localhost/prueba?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700'
+
